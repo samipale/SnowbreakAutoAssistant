01ca1813dd7a13fabf7443f85cecb9d9d887c2d9
samipale <seisei_126@163.com>
2025-05-10 10:26:59 +0800

    Update __init__.py

--- app/modules/ocr/__init__.py
+++ app/modules/ocr/__init__.py
@@ -5,7 +5,7 @@ import json
 from app.common.logger import logger
 from app.modules.ocr.ocr import OCR
 
-with open("AppData/ocr_replacements.json", 'r', encoding='utf-8') as file:
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../AppData/ocr_replacements.json"), 'r', encoding='utf-8') as file:
     replacements = json.load(file)
 
 # 初始化 OCR 对象

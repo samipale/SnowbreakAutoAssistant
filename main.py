22b25c79843bc890f45b8a1f54913e26c8c4dd51
samipale <seisei_126@163.com>
2025-05-10 11:48:38 +0800

    windows支持

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # coding:utf-8
 import os
 import sys
+import ctypes
+
+# 检查是否为管理员权限，如果不是则启动管理员权限
+if not ctypes.windll.shell32.IsUserAnAdmin():
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    sys.exit()
 
 from PyQt5.QtCore import Qt, QTranslator
 from PyQt5.QtGui import QFont
@@ -9,6 +15,9 @@ from qfluentwidgets import FluentTranslator
 
 from app.common.config import config
 from app.view.main_window import MainWindow
+
+# 切换到脚本目录
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  
 
 # enable dpi scale
 if config.get(config.dpiScale) != "Auto":
